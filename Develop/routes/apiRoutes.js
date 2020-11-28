65f2d0f298baf1f8643eb7acc53f81cac344033a
Daoyuan Guo <linkingarden@foxmail.com>
2020-11-28 03:07:13 -0800

    fixing some bugs

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,35 +4,46 @@ const path = require("path");
 // ROUTING
 
 module.exports = app => {
-    fs.readFile("db/db.json", "utf8", (err, data) =>{
-       if(err) throw err;
-       var notes = JSON.parse(data);
-       
-       app.get("/api/notes", function(req, res){
-           return res.json(notes);
-       });
 
-       app.post("/api/notes", function(req, res){
-           let newNote = req.body;
-           notes.push(newNote);
-           updateNDB();
-           console.log("New Note Added");
-       });
+    var notes = JSON.parse(fs.readFileSync("./db/db.json"));
 
-       app.delete("/api/notes/:id", function(req, res){
-           notes.splice(req.params.id, 1);
-           updateNDB();
-           console.log("Note deleted");
-       });
-
-       function updateNDB(){
-           fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err=>{
-               if (err) throw err;
-               //res.send();
-               return true;
-           });
-
-       };
-
+    app.get("/api/notes", function (req, res) {
+        res.json(notes);
     });
+
+    app.post("/api/notes", function (req, res) {
+        let newNote = req.body;
+
+        newNote.id = notes.length + 1;
+        notes.push(newNote);
+        updateNDB();
+        console.log("New Note Added");
+    });
+
+    app.get("/api/notes/:id", function (req, res) {
+        res.json(notes[req.params.id]);
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+        notes.splice(req.params.id, 1);
+        if(notes){
+            for(let i = 0; i < notes.length; i++)
+            {
+                notes[i].id = i;
+            }
+        }
+        updateNDB();
+        console.log("Note deleted");
+    });
+
+    function updateNDB() {
+        fs.writeFile("db/db.json", JSON.stringify(notes), err => {
+            if (err) throw err;
+            //res.send();
+            return true;
+        });
+
+    };
+
+
 }
